cd67b7bba1ad113b377875e347d2deddfcddd3e5
Aurora Bogantes <aurorabogantes@gmail.com>
2025-01-25 14:54:18 -0600

    Create MarioBros.java

---
+++ mariobros/MarioBros.java
@@ -0,0 +1,11 @@
+package mariobros;
+
+import vista.Ventana;
+import vista.Juego;
+
+public class MarioBros {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        Juego juego = new Juego(); 
+    }
+}
